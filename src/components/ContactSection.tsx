75ea9289fefa97886064d1ba536a1f17675fe3c0
Belkhelfa Mehdi <mehdibelkhelfa6@gmail.com>
2026-05-05 14:56:08 +0200

    refactor: update icon imports in ContactSection for dynamic loading

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { FaLinkedinIn, FaGithub, FaGoogle } from "react-icons/fa";
 import { contacts } from "../constants/content";
 
+const Fa = (await import("react-icons/fa")).default as unknown as Record<string, React.ComponentType<{ className?: string }>>;
+
 const iconMap: Record<string, React.ReactElement> = {
-  linkedin: <FaLinkedinIn className="w-6 h-6" />,
-  github: <FaGithub className="w-6 h-6" />,
-  gmail: <FaGoogle className="w-6 h-6" />,
+  linkedin: React.createElement(Fa.FaLinkedinIn, { className: "w-6 h-6" }),
+  github: React.createElement(Fa.FaGithub, { className: "w-6 h-6" }),
+  gmail: React.createElement(Fa.FaGoogle, { className: "w-6 h-6" }),
 };
 
 const ContactSection: React.FC = () => {
